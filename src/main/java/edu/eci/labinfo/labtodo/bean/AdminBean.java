6e6ccdc6bd07f451792f0f3364f82ab99071ae2e
Daniel Santanilla <daniel1sa_@hotmail.com>
2023-10-12 15:38:15 -0500

    Logica para usuario administrador

---
+++ src/main/java/edu/eci/labinfo/labtodo/bean/AdminBean.java
@@ -0,0 +1,91 @@
+package edu.eci.labinfo.labtodo.bean;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContextWrapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import edu.eci.labinfo.labtodo.model.Status;
+import edu.eci.labinfo.labtodo.model.Task;
+import edu.eci.labinfo.labtodo.service.PrimeFacesWrapper;
+import edu.eci.labinfo.labtodo.service.TaskService;
+
+@Component
+@ManagedBean(name = "adminBean")
+@SessionScoped
+public class AdminBean {
+    
+
+    @Autowired
+    TaskService taskService;
+
+    @Autowired
+    static FacesContextWrapper facesContextWrapper;
+
+    @Autowired
+    PrimeFacesWrapper primeFacesWrapper;
+
+    private List<Task> selectedTasks;
+    private String newState = "";
+
+    public List<Task> getSelectedTasks() {
+        return selectedTasks;
+    }
+
+    public void setSelectedTasks(List<Task> selectedTasks) {
+        this.selectedTasks = selectedTasks;
+    }
+
+    public String getNewState() {
+        return newState;
+    }
+
+    public void setNewState(String newState) {
+        this.newState = newState;
+    }
+
+    public Boolean modifyStateTaks() {
+        if (this.newState == null || this.newState.isEmpty()) {
+            facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Por favor, selecciona un estado.", "Error"));
+            primeFacesWrapper.current().ajax().update(":form:messages");
+            return false;
+        }
+        try {
+            for (Task task : selectedTasks) {
+                task.setStatus(this.newState);
+                taskService.updateTask(task);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            int size = this.selectedTasks.size();
+            String summary = size > 1 ? size + " tareas actualizadas con exito" : size + " tarea actualizada con exito";
+            facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, summary, "Exito"));
+            primeFacesWrapper.current().ajax().update(":form:dt-task", ":form:messages");
+        }
+        return true;
+    }
+
+    public String getUpdateButtonMessage() {
+        String message = "Cambiar estado de";
+        if (hasSelectedTasks()) {
+            int size = this.selectedTasks.size();
+            return size > 1 ? message + " " + size + " tareas seleccionadas" : message + " 1 tarea seleccionada";
+        }
+        return message;
+    }
+
+    public boolean hasSelectedTasks() {
+        return this.selectedTasks != null && !this.selectedTasks.isEmpty();
+    }
+
+    
+
+}
