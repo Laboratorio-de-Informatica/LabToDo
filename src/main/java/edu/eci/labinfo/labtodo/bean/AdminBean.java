b8ab02e9d72f4cf5f2ae48795271574b015986b3
Daniel Santanilla <daniel1sa_@hotmail.com>
2023-10-15 00:08:15 -0500

    Editar roles de usuarios

--- src/main/java/edu/eci/labinfo/labtodo/bean/AdminBean.java
+++ src/main/java/edu/eci/labinfo/labtodo/bean/AdminBean.java
@@ -10,9 +10,12 @@ import javax.faces.context.FacesContextWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import edu.eci.labinfo.labtodo.model.Role;
 import edu.eci.labinfo.labtodo.model.Task;
+import edu.eci.labinfo.labtodo.model.User;
 import edu.eci.labinfo.labtodo.service.PrimeFacesWrapper;
 import edu.eci.labinfo.labtodo.service.TaskService;
+import edu.eci.labinfo.labtodo.service.UserService;
 
 @Component
 @ManagedBean(name = "adminBean")
@@ -23,13 +26,18 @@ public class AdminBean {
     TaskService taskService;
 
     @Autowired
+    UserService userService;
+
+    @Autowired
     static FacesContextWrapper facesContextWrapper;
 
     @Autowired
     PrimeFacesWrapper primeFacesWrapper;
 
     private List<Task> selectedTasks;
+    private List<User> selectedUsers;
     private String newState = "";
+    private String newRole = "";
 
     public List<Task> getSelectedTasks() {
         return selectedTasks;
@@ -39,12 +47,28 @@ public class AdminBean {
         this.selectedTasks = selectedTasks;
     }
 
+    public List<User> getSelectedUsers() {
+        return selectedUsers;
+    }
+
+    public void setSelectedUsers(List<User> selectedUsers) {
+        this.selectedUsers = selectedUsers;
+    }
+
     public String getNewState() {
         return newState;
     }
 
     public void setNewState(String newState) {
         this.newState = newState;
+    }
+
+    public String getNewRole() {
+        return newRole;
+    }
+
+    public void setNewRole(String newRole) {
+        this.newRole = newRole;
     }
 
     /**
@@ -69,9 +93,38 @@ public class AdminBean {
         } finally {
             int size = this.selectedTasks.size();
             String summary = size > 1 ? size + " tareas actualizadas con exito" : size + " tarea actualizada con exito";
-            facesContextWrapper.getCurrentInstance().addMessage(null,
-                    new FacesMessage(FacesMessage.SEVERITY_INFO, summary, "Exito"));
+            facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, summary, "Exito"));
             primeFacesWrapper.current().ajax().update(":form:dt-task", ":form:messages");
+            selectedTasks.clear();
+        }
+        return true;
+    }
+
+    /**
+     * Metodo que cambia el rol de los usuarios seleccionados.
+     * 
+     * @return true si se cambio el rol de los usuarios, false de lo contrario.
+     */
+    public Boolean modifyUserRole() {
+        if (this.newRole == null || this.newRole.isEmpty()) {
+            facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Por favor, selecciona un rol de usuario.", "Error"));
+            primeFacesWrapper.current().ajax().update("form:messages");
+            return false;
+        }
+        try {
+            for (User user : selectedUsers) {
+                user.setUserRole(Role.findByValue(newRole).getValue());
+                userService.updateUser(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            int size = this.selectedUsers.size();
+            String summary = size > 1 ? size + " usuarios actualizados con exito" : size + " usuario actualizado con exito";
+            selectedUsers.clear();
+            facesContextWrapper.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, summary, "Exito"));
+            primeFacesWrapper.current().ajax().update("form:users-list", "form:messages", ":role-label", "form:edit-users-button");
         }
         return true;
     }
@@ -92,12 +145,36 @@ public class AdminBean {
     }
 
     /**
+     * Metodo que retorna el mensaje que se muestra en el boton de cambio de rol
+     * para cambiar el rol de los usuarios seleccionados.
+     * 
+     * @return mensaje que se muestra en el boton de cambio de rol.
+     */
+    public String getRoleButtonMessage() {
+        String message = "Cambiar rol de";
+        if (hasSelectedUsers()) {
+            int size = this.selectedUsers.size();
+            return size > 1 ? message + " " + size + " usuarios seleccionados" : message + " 1 usuario seleccionado";
+        }
+        return message;
+    }
+
+    /**
      * Metodo que retorna true si hay tareas seleccionadas, false de lo contrario.
      * 
      * @return true si hay tareas seleccionadas, false de lo contrario.
      */
     public boolean hasSelectedTasks() {
         return this.selectedTasks != null && !this.selectedTasks.isEmpty();
+    }
+
+    /**
+     * metodo que retorna true si hay usuarios seleccionados, false de lo contrario.
+     * 
+     * @return true si hay usuarios seleccionados, false de lo contrario.
+     */
+    public boolean hasSelectedUsers() {
+        return this.selectedUsers != null && !this.selectedUsers.isEmpty();
     }
 
 }
