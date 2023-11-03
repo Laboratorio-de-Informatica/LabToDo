6a90a8a14467ddc73ec64e83c09a580d5fe3381b
Daniel Santanilla <daniel.santanilla@mail.escuelaing.edu.co>
2023-11-03 17:31:13 -0500

    Actualizar modelos y dependencias

---
+++ src/main/java/edu/eci/labinfo/labtodo/model/AccountType.java
@@ -0,0 +1,37 @@
+package edu.eci.labinfo.labtodo.model;
+
+public enum AccountType {
+
+    SIN_VERIFICAR("Sin verificar"),
+    ACTIVO("Activo"),
+    INACTIVO("Inactivo");
+
+    private String value;
+
+    AccountType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Busca un tipo de cuenta por su valor, insensible a mayúsculas y minúsculas.
+     *
+     * @param accountType El valor del rol a buscar.
+     * @return El tipo de cuenta correspondiente al valor proporcionado, o null si no se
+     * encuentra.
+     */
+    public static AccountType findByValue(String accountType) {
+        AccountType response = null;
+        for (AccountType r : AccountType.values()) {
+            if (r.getValue().equalsIgnoreCase(accountType)) {
+                response = r;
+                break;
+            }
+        }
+        return response;
+    }
+
+}
